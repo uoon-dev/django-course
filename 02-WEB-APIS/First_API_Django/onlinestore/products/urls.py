8a3cb9221a44669d156b31893f07da3533dc4465
fox <uoon.dev@gmail.com>
2019-07-26 23:09:17 +0900

    add manufacturer api

--- 02-WEB-APIS/First_API_Django/onlinestore/products/urls.py
+++ 02-WEB-APIS/First_API_Django/onlinestore/products/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import product_list, product_detail #ProductDetailView, ProductListView
+from .views import (manufacturer_detail, manufacturer_list, 
+  product_detail, product_list) #ProductDetailView, ProductListView
 
 urlpatterns = [
   path("products/", product_list, name="product-list"),
   path("products/<int:pk>/", product_detail, name="product-detail"),
+  path("manufacturer/", manufacturer_list, name="manufacturer-list"),
+  path("manufacturer/<int:pk>/", manufacturer_detail, name="manufacturer-detail"),
 ]
 
 # urlpatterns = [
