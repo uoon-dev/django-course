0c1ddb6d70afc17c45176a2ce0aaa7ad03d76e7e
fox <uoon.dev@gmail.com>
2019-08-05 19:43:32 +0900

    make quotes api

---
+++ 04-DRF-LEVEL-TWO/quotesapi/quotes/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+
+from quotes.models import Quote
+from quotes.api.permissions import IsAdminUserOrReadOnly
+from quotes.api.serializer import QuoteSerializer
+
+class QuoteListCreateAPIView(generics.ListCreateAPIView):
+  queryset = Quote.objects.all().order_by("-id")
+  serializer_class = QuoteSerializer
+  permission_classes = [IsAdminUserOrReadOnly]
+
+class QuoteDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Quote.objects.all()
+  serializer_class = QuoteSerializer
+  permission_classes = [IsAdminUserOrReadOnly]
