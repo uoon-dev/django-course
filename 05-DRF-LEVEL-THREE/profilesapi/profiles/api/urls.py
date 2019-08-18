35d6981937c4d40e9c17545d3ca8a8b608adf5fc
fox <uoon.dev@gmail.com>
2019-08-18 23:49:16 +0900

    filter and search

--- 05-DRF-LEVEL-THREE/profilesapi/profiles/api/urls.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/api/urls.py
@@ -7,7 +7,7 @@ from profiles.api.views import (ProfileViewSet, ProfileStatusViewSet, AvatarUpda
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
-router.register(r"status", ProfileStatusViewSet)
+router.register(r"status", ProfileStatusViewSet, basename="status")
 
 urlpatterns = [
   path("", include(router.urls)),
--- 05-DRF-LEVEL-THREE/profilesapi/profiles/api/views.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/api/views.py
@@ -1,9 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets, mixins
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 # from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import viewsets
-from rest_framework import mixins
 from profiles.models import Profile, ProfileStatus
 from profiles.api.permissions import IsOwnProfileOrReadOnly, IsOwnerOrReadOnly
 from profiles.api.serializers import (ProfileSerializer, ProfileStatusSerializer, ProfileAvatarSerializer)
@@ -24,11 +23,20 @@ class ProfileViewSet(mixins.UpdateModelMixin,
   queryset = Profile.objects.all()
   serializer_class = ProfileSerializer
   permission_classes = [IsAuthenticated, IsOwnProfileOrReadOnly]
+  filter_backends = [SearchFilter]
+  search_fields = ["city"]
 
 class ProfileStatusViewSet(ModelViewSet):
   queryset = ProfileStatus.objects.all()
   serializer_class = ProfileStatusSerializer
   permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+  def get_queryset(self):
+    queryset = ProfileStatus.objects.all()
+    username = self.request.query_params.get("username", None)
+    if username is not None:
+      queryset = queryset.filter(user_profile__user__username=username)
+    return queryset
 
   def perform_create(self, serializer):
     user_profile = self.request.user.profile
